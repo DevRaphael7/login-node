10e2b8eacb5debdecaee45606181cbedcc785f3c
RAPHAEL RAMALHO DOS SANTOS <raphael2000840@creasp.org.br>
2022-07-13 08:38:20 -0300

    Teste

--- src/controllers/loginController/login.controller.ts
+++ src/controllers/loginController/login.controller.ts
@@ -9,6 +9,7 @@ export class LoginController {
 
     constructor() {
         this.tokenService = new TokenService();
+        //Teste
     }
 
     public loginController = (req: Request, res: Response) => {
